d3134c24f0fd32776d60c904c562ba10731d83eb
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-06-29 03:43:02 +0530

    new changes

--- backend/src/DBconfig/dbconfig.ts
+++ backend/src/DBconfig/dbconfig.ts
@@ -4,6 +4,10 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: "localhost",
+  url: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   port: 5432,
   username: "postgres",
   password: "isma@123",
