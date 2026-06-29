d29ff62fac411b711505523b5c3458b649b2dad2
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-06-30 02:18:11 +0530

    Use Neon DATABASE_URL

--- backend/src/DBconfig/dbconfig.ts
+++ backend/src/DBconfig/dbconfig.ts
@@ -1,19 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
   url: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-  port: 5432,
-  username: "postgres",
-  password: "isma@123",
-  database: "lms",
   synchronize: true,
-  entities: ["src/entities/**/*.ts"],
-  migrations: ["src/migration/**/*.ts"],
-  subscribers: ["src/subscribers/**/*.ts"],
+  entities: ["dist/entities/**/*.js"],
+  migrations: ["dist/migration/**/*.js"],
+  subscribers: ["dist/subscribers/**/*.js"],
 });
