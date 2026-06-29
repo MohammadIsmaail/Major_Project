5b2bb7653b5d840a5522d45a7b2f40edb0ad1eb9
Mohammad Ismaail <mohammadismaail687@gmail.com>
2026-06-30 01:37:49 +0530

    new changes

--- frontent/src/redux/rootreducer.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import authslice from "./slice/authslice";
-
-const rootReducer=combineReducers({
-    auth:authslice,
-})
-
-export default rootReducer
